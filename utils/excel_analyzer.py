762c1769c8057cd483bb59c2a4efe5f2653f1125
klebertxquer <kleber.b4nk@gmail.com>
2025-06-21 22:51:16 -0300

    Update excel_analyzer.py

--- utils/excel_analyzer.py
+++ utils/excel_analyzer.py
@@ -1,1 +1,9 @@
+import pandas as pd
+
+def carregar_excel(caminho):
+    df = pd.read_excel(caminho)
+    return df
+
+def filtrar_por_vendedor(df, nome_vendedor):
+    return df[df['Vendedor'].str.contains(nome_vendedor, case=False, na=False)]
 
