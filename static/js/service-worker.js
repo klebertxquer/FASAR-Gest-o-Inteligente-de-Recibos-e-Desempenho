c1626df9a077418cb692bd7ac87fdb6b57301308
klebertxquer <kleber.b4nk@gmail.com>
2025-06-23 10:42:52 -0300

    Update service-worker.js

--- static/js/service-worker.js
+++ static/js/service-worker.js
@@ -1,17 +1,24 @@
-  "service-worker.js": """
-    const CACHE_NAME = 'fasar-cache-v1';
-    const urlsToCache = [
-      '/',
-      '/static/css/style.css',
-      '/static/js/main.js',
-      '/static/icons/icon-192.png',
-      '/static/icons/icon-512.png'
-    ];
+const CACHE_NAME = 'fasar-cache-v1';
+const urlsToCache = [
+  '/',
+  '/static/css/style.css',
+  '/static/js/main.js',
+  '/static/icons/icon-192.png',
+  '/static/icons/icon-512.png'
+];
 
-    self.addEventListener('install', event => {
-      event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-          return cache.addAll(urlsToCache);
-        })
-      );
-    });
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
